687b1f897200ef51ec8e97963ee1d889bdf1fd9e
Devon O'Rourke <devon.orourke@gmail.com>
2020-10-29 13:37:29 -0400

    Update get_beta_group_significance_windows2016data.sh

--- scripts/shell_scripts/get_beta_group_significance_windows2016data.sh
+++ scripts/shell_scripts/get_beta_group_significance_windows2016data.sh
@@ -9,6 +9,7 @@
 wget https://raw.githubusercontent.com/devonorourke/nhguano/master/data/text_tables/trees/finalfiltd_rootedtree.nwk
 qiime tools import --input-path finalfiltd_rootedtree.nwk --output-path finalfiltd_rooted_tree.qza --type 'Phylogeny[Rooted]'
 
+
 ORIDIR=$(pwd)
 
 for PREFIX in $(printf "win456\nwin567\n"); do
@@ -32,8 +33,9 @@ for PREFIX in $(printf "win456\nwin567\n"); do
     qiime diversity beta-phylogenetic --i-table "$PREFIX"_OTUtable.qza --i-phylogeny finalfiltd_rooted_tree.qza --p-metric 'unweighted_unifrac' --o-distance-matrix "$PREFIX"_dist_uuni.qza
     qiime diversity beta-phylogenetic --i-table "$PREFIX"_OTUtable.qza --i-phylogeny finalfiltd_rooted_tree.qza --p-metric 'weighted_unifrac' --o-distance-matrix "$PREFIX"_dist_wuni.qza
 
-  ## run beta group significance for each distance; once for 'Window' and once for 'Site' variables
+## run beta group significance for each distance; once for 'Window' and once for 'Site' variables
   ## export each beta group significance visualization, collect raw text data, and add
+  #### for site factorgroup
     for METRIC in $(printf "dice\nbray\nuuni\nwuni\n"); do
       qiime diversity beta-group-significance \
         --i-distance-matrix "$PREFIX"_dist_"$METRIC".qza \
@@ -44,15 +46,29 @@ for PREFIX in $(printf "win456\nwin567\n"); do
         --output-path "$PREFIX"_betasig_site_"$METRIC"_output
       cut -f 2-6 ./"$PREFIX"_betasig_site_"$METRIC"_output/raw_data.tsv | \
       awk -v var=$METRIC 'NR>1 {print $0"\t"var}' | \
-      awk -v var=$PREFIX '{print $0"\t"var}' > "$PREFIX"_betasig_site_"$METRIC"_data.tsv
+      awk -v var=$PREFIX '{print $0"\t"var"\t""site"}' > "$PREFIX"_betasig_site_"$METRIC"_data.tsv
+    #### for window factorgroup
+      qiime diversity beta-group-significance \
+        --i-distance-matrix "$PREFIX"_dist_"$METRIC".qza \
+        --m-metadata-file "$PREFIX"meta_forQIIME.txt \
+        --m-metadata-column 'Window' --o-visualization "$PREFIX"_betasig_window_"$METRIC".qzv;
+      qiime tools export \
+        --input-path "$PREFIX"_betasig_window_"$METRIC".qzv \
+        --output-path "$PREFIX"_betasig_window_"$METRIC"_output
+      cut -f 2-6 ./"$PREFIX"_betasig_window_"$METRIC"_output/raw_data.tsv | \
+      awk -v var=$METRIC 'NR>1 {print $0"\t"var}' | \
+      awk -v var=$PREFIX '{print $0"\t"var"\t""window"}' > "$PREFIX"_betasig_window_"$METRIC"_data.tsv
+      
     done
 
-    ## combine all outputs into single file for particular window group:
-  printf 'SubjectID1\tSubjectID2\tGroup1\tGroup2\tDistance\tMetric\tWindowGroup\n' > "$PREFIX"_betasig_site_allMetrics_data.tsv
+    ## combine all outputs into single file for particular group (win456 or win567):
+  printf 'SubjectID1\tSubjectID2\tGroup1\tGroup2\tDistance\tMetric\tWindowGroup\nFactorGroup\n' > "$PREFIX"_betasig_site_allMetrics_data.tsv
 
   cat "$PREFIX"_betasig_site_dice_data.tsv "$PREFIX"_betasig_site_bray_data.tsv \
   "$PREFIX"_betasig_site_uuni_data.tsv "$PREFIX"_betasig_site_wuni_data.tsv \
-  >> "$PREFIX"_betasig_site_allMetrics_data.tsv
+  "$PREFIX"_betasig_window_dice_data.tsv "$PREFIX"_betasig_window_bray_data.tsv \
+  "$PREFIX"_betasig_window_uuni_data.tsv "$PREFIX"_betasig_window_wuni_data.tsv \
+  >> "$PREFIX"_betasig_allMetrics_data.tsv
 
   cd $ORIDIR
 
