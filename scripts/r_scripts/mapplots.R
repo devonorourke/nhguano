b7b0269b68aac5077145f80a60a7cb64d163824c
Devon O'Rourke <devon.orourke@gmail.com>
2020-11-16 15:30:07 -0500

    add supp dir

--- scripts/r_scripts/mapplots.R
+++ scripts/r_scripts/mapplots.R
@@ -1,65 +1,188 @@
-## ggmap for plotting diet info:
-## see here for ggmap program: https://github.com/dkahle/ggmap
-## see here for useful static and animated ggmap applications:
-  ## http://www.bernhardlearns.com/2018/11/animating-r-plot-with-gganimate.html
-## needed to register to access Google API to run ggmap; start here: https://console.cloud.google.com
-  ## once API key created, in R runonce: register_google(key = "{enter_your_long_key}", account_type = "standard", write = TRUE)
-
 library(ggmap)
 library(tidyverse)
-library(formattable)
-library(htmltools)
-library(webshot)
+library(urbnmapr)
+library(cowplot)
+library(lubridate)
 
-## import metadata
-metadata <- as.data.frame(read_csv(file="~/Repos/nhguano/data/metadata/nhbat_meta.csv"))
+## import site late/long vals only for samples used in diet analysis (not all samples collected)
+site_data <- as.data.frame(read_csv(file="/Users/devonorourke/Documents/nau_projects/guano/NHguano_redux/redo_taxa_boldANML/min1kseqs_Samps_OTUtable_long_wTaxa_wDateBins.csv")) %>% 
+  distinct(Site, SiteLat, SiteLong) %>% 
+  rename(lat = 'SiteLat', lon = 'SiteLong')
 
-################################################################################
-## part 1 - summary table of collections at each site
-################################################################################
+## define boundaries for base map
+## could use just those areas we sampled within NH, but get's kinda too zoomed in...
+# maxlat = max(metadata$SiteLat) + 0.15
+# minlat = min(metadata$SiteLat) - 0.15
+# maxlon = max(metadata$SiteLong) + 0.15
+# minlon = min(metadata$SiteLong) - 0.15
+# nhlatlonbox <- c(left = minlon, bottom = minlat, right = maxlon, top = maxlat)
 
-## summarise number of samples collected at each site
-collection_sumry <- as.data.frame(metadata %>% 
-  group_by(Site, StudyID, SiteLat, SiteLong) %>% 
-  summarise(Samples=n_distinct(SampleID), Weeks=n_distinct(WOY)) %>% 
-  arrange(Site, StudyID)) %>% 
-  mutate(Year=ifelse(StudyID=="oro15", "2015", "2016"))
+## use a broader boundary to show most of NH instead of zooming into just southern half (makes inset map easier to understand)
+alt_NHwide_boundary <- c(left = -72.6, bottom = 42.5, right = -70.5, top = 44.5)
 
-## formattable object to save
-csumtable <- formattable(collection_sumry %>% 
-              select(Site, Year, Weeks, Samples),
-            list(Samples = color_tile("white", "orange"),
-                 Weeks = color_tile("white", "dodgerblue2")))
-
-## export formattable object because table is too long for basic R export
-## save table as 'alldata_collectionSummary'; 
-  ## modify the "width = 30%" value if you want to stretch/shrink the spacing between columns
-export_formattable <- function(f, file, width = "30%", height = NULL,
-                               background = "white", delay = 0.2)
-{
-  w <- as.htmlwidget(f, width = width, height = height)
-  path <- html_print(w, background = background, viewer = NULL)
-  url <- paste0("file:///", gsub("\\\\", "/", normalizePath(path)))
-  webshot(url,
-          file = file,
-          selector = ".formattable_widget",
-          delay = delay)
-}
-
-## export here; save as 'alldata_collectionSummary'
-setwd("~/Repos/nhguano/figures/")
-export_formattable(csumtable,"alldata_collectionSummary.png")
-
-################################################################################
-## part 2 - map of collection locations
-################################################################################
+## generate base map for plot
+p_baseNHmap <- get_stamenmap(bbox = alt_NHwide_boundary,
+              zoom = 10, 
+              #maptype = "toner-background", ## shows roads and lakes ok, but would be better without roads
+              maptype = "terrain-background",
+              color = "color") %>% 
+  ggmap(darken = c(0.2, "white")) ## increase number for more transparent color
 
 
-## define boundaries?
-nh <- c(left = -73, bottom = 42.5, right = -70.5, top = 45.5)
+## add in metadata to base map:
+closeSites = c("CNA", "CNB", "WLT", "MAP", "BRN")
+pNHwithlabs <- p_baseNHmap + 
+  geom_label(data = site_data %>% filter(!Site %in% closeSites), aes(x=lon, y=lat, label=Site), size=3.5) +
+  geom_label_repel(data = site_data %>% filter(Site %in% closeSites), aes(x=lon, y=lat, label=Site), size=3.5, direction = "x", segment.colour = "gray30")
 
-## get directrly from Google
-get_googlemap(center = c(-71.768521,43.395290), 
-              zoom = 9, color = "color") %>% ggmap()
+pNHwithlabs
+ggsave(filename = "~/github/nhguano/figures/figure1_nhmapNoSampleSizes.png", width=8, height=11, dpi=150)
+ggsave(filename = "~/github/nhguano/figures/figure1_nhmapNoSampleSizes.svg", width=8, height=11, dpi=300)
 
-## see also: markers (add metadata markers with Site codes); path (layer data)
+#### get map of northeast USA to pin NH into this map at top right:
+urbnmapr_data = urbnmapr::states
+stateNamesToKeep <- c('Maine', 'New Hampshire', 'Vermont', 'New York', 'Massachusetts', 'Connecticut')
+new_data <- urbnmapr_data %>% filter(state_name %in% stateNamesToKeep)
+
+p_northeast <- ggplot() + 
+  geom_polygon(data = new_data, mapping = aes(x = long, y = lat, group = group),
+               fill = 'gray50', color = 'white') +
+  geom_polygon(data = new_data %>% filter(state_name == "New Hampshire"), 
+               mapping = aes(x = long, y = lat, group = group), fill = 'black', color = 'white') +
+  #coord_map(projection = "albers", lat0 = 39, lat1 = 45) +
+  theme_nothing() +
+  theme(plot.background = element_rect(fill = "gray80", color = "black"))
+
+#### now stitch those two together into a single plot:
+ggdraw() +
+  draw_plot(pNHwithlabs) +
+  draw_plot(plot = p_northeast,
+            x = 0.5,
+            y = 0.65, 
+            width = 0.42,
+            height = 0.42,
+            scale = 0.5)
+
+ggsave(filename = "~/github/nhguano/figures/figure1_nhmapNoSampleSizes_withInset.png", height = 8, width = 11, dpi=150)
+ggsave(filename = "~/github/nhguano/figures/figure1_nhmapNoSampleSizes_withInset.pdf", height = 8, width = 11, dpi=300)
+
+## this image will be used in Figure 1, as panel "A", in addition to the landscape cover barplot generated in `spatial_work_NHguano.R`
+
+#############
+## let's also include a summary of the data collected vs. processed:
+## using raw collection records to determine number of guano samples collected (not necessarily sequenced!)
+## and certainly not necessarily sequenced AND generated enough data to be included in analaysis
+
+## import 2015 and 2016 data
+nhsites2016 <- c("ALS", "ROL", "COR", "MAP", "BRN", "MAS", "GIL", "MTV", "FOX", "CNB", "HOL", "PEN", 'CHI', 'EPS', 'CNA', 'HOP')
+
+rawmeta2015 <- read_csv(file="~/Downloads/NHraw2015collectiondata.csv") %>% 
+  rename(Site = "Location name", CollectionDate = "Collection Date", SampleID = `Sample ID`) %>% 
+  select(-Box, -`WEEK OF YEAR`, -status, -PlateNumber, -X8) %>% 
+  mutate(Site = case_when(Site == "maple hill" ~ "MAP",
+                          Site == "Hopkinton" ~ "HOP",
+                          Site == "massabesic" ~ "MAS",
+                          Site == "willard" ~ "WLD", 
+                          Site == "brown lane" ~ "BRN",
+                          Site == "fox state" ~ "FOX",
+                          Site == "wilton" ~ "WLT",
+                          Site == "Cornish" ~ "COR",
+                          Site == "Greenfield" ~ "GRN",
+                          Site == "Swanzey" ~ "SWZ",
+                          Site == "gilsum" ~ "GIL",
+                          TRUE ~ as.character(Site))) %>% 
+  filter(Site != "unknown") %>% filter(!is.na(Site)) %>% ## drop any samples with unknown/missing Site info
+  mutate(Date = mdy(CollectionDate),
+         Year = year(Date),
+         SampleID = paste0("oro15", SampleID)) %>% 
+  select(-CollectionDate)
+
+rawmeta2016 <- read_csv(file="~/Downloads/NHraw2016collectiondata.csv") %>% 
+  select(StudyID, SampleID, LocationName, CollectionDate) %>% 
+  mutate(Date = mdy(CollectionDate),
+         Year = year(Date)) %>% 
+  select(-CollectionDate) %>% 
+  filter(LocationName %in% nhsites2016) %>% 
+  filter(StudyID == "oro16") %>% 
+  filter(!grepl("contaminated", SampleID)) %>%  ## discard the few contaminated samples
+  mutate(SampleID = paste0("oro16", SampleID),
+         Year = ifelse(is.na(Year), 2016, Year)) %>% 
+  rename(Site = "LocationName") %>% 
+  select(-StudyID)
+
+rawmetaAll <- rbind(rawmeta2015, rawmeta2016)
+rm(rawmeta2015, rawmeta2016)
+
+## count how many samples were collected in each dataset:
+allmeta_sampleSumry <- rawmetaAll %>% 
+  group_by(Year, Site) %>% 
+  summarise(SamplesCollected = n_distinct(SampleID)) %>% 
+  pivot_wider(names_from = Year, values_from = SamplesCollected) %>% 
+  rename(collected2015 = `2015`, collected2016 = `2016`)
+
+## bring in seq data relevant to these samples... how many of these samples ended up generating sufficient seq data used in our diet analyses?
+allrawSamplesWithSeqData <- read_csv("/Users/devonorourke/Documents/nau_projects/guano/NHguano_redux/redo_taxa_boldANML/min1kseqs_Samps_OTUtable_long_wTaxa_wDateBins.csv")
+allmeta_seqSumry <- allrawSamplesWithSeqData %>% 
+  group_by(Year, Site) %>% 
+  summarise(SamplesAnalyzed = n_distinct(SampleID)) %>% 
+  pivot_wider(names_from = Year, values_from = SamplesAnalyzed) %>% 
+  rename(evaluated2015 = `2015`, evaluated2016 = `2016`)
+
+allmeta_fullSumry <- merge(allmeta_sampleSumry, allmeta_seqSumry, all = TRUE) %>% 
+  relocate(Site, collected2015, evaluated2015, collected2016, evaluated2016) %>% 
+  mutate_all(funs(replace_na(.,"..")))
+
+write_csv(allmeta_fullSumry,
+          path="~/github/nhguano/supplementaryData/tableS2_samplesPerSiteSumry.csv")
+
+
+rm(allrawSamplesWithSeqData)
+rm(allmeta_sampleSumry, allmeta_seqSumry)
+
+
+#### unused code
+# ## summarise number of samples collected at each site in each year in wide format for exportable .csv
+# collection_sumry_wide <- metadata %>% 
+#   group_by(Site, Year) %>% 
+#   summarise(Samples=n_distinct(SampleID)) %>% 
+#   arrange(Site, Year) %>% 
+#   ungroup() %>%
+#   pivot_wider(values_from = "Samples", names_from="Year", values_fill=0) %>% 
+#   relocate(Site, `2015`, `2016`)
+
+# get_stamenmap(bbox = alt_NHwide_boundary,
+#               zoom = 10, 
+#               #maptype = "toner-background", ## shows roads and lakes ok, but would be better without roads
+#               maptype = "toner-background",
+#               color = "color") %>% ggmap()
+
+
+## summarise number of samples collected at each site in each year in long format for plotting
+# collection_sumry_long <- metadata %>% 
+#   group_by(Site, Year, SiteLat, SiteLong) %>% 
+#   summarise(Samples=n_distinct(SampleID)) %>% 
+#   arrange(Site, Year) %>% 
+#   rename(lon = "SiteLong", lat = "SiteLat") %>% 
+#   ungroup()
+
+# tmp2 <- metadata %>% 
+#   group_by(Site, Year, SiteLat, SiteLong) %>% 
+#   summarise(Samples=n_distinct(SampleID)) %>% 
+#   arrange(Site, Year) %>% 
+#   rename(lon = "SiteLong", lat = "SiteLat") %>% 
+#   ungroup() %>% 
+#   mutate(Year = as.character(Year)) %>% 
+#   mutate(lon = ifelse(Year == "2015", lon+0.05, lon-0.05))
+
+## relabel metadata for better plot labels:
+# tmp <- metadata %>% 
+#   rename(lat = "SiteLat", lon = "SiteLong") %>% 
+#   group_by(Site, Year, lat, lon) %>% 
+#   summarise(Samples=n_distinct(SampleID)) %>% 
+#   arrange(Site, Year) %>% 
+#   pivot_wider(values_from = "Samples", names_from="Year") %>% 
+#   #mutate(Site = as.factor(Site)) %>% 
+#   mutate(LabelVal = case_when(is.na(`2016`) ~ paste0(Site, "\n", `2015`,"*"),
+#                               is.na(`2015`) ~ paste0(Site, "\n", `2016`, "**"),
+#                               TRUE ~ paste0(Site, "\n", `2015`, "*|", `2016`,"**"))) %>% 
+#   select(-`2016`, -`2015`)
